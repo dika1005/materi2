8eb2f17642dbeae5ca031699e4b5c591db5c19d0
dika ramadani <dikabangkok21@gmail.com>
2024-11-09 21:50:16 +0700

    first commit

---
+++ p1.js
@@ -0,0 +1,62 @@
+// if(true) {
+//      console.log("nilai true");
+//     }   
+
+// if(false) {
+//      console.log("nilai else");
+//  }
+
+// if (true) {
+//     console.log("nilai true");
+// } else {
+//     console.log("nilai else");
+// }
+
+// if (false) {
+//     console.log("nilai true");
+// } else {
+//     console.log("nilai else");
+// }
+
+// const test1 = 1;
+// const test2 = 2;
+// const test3 = "2";
+
+// console.log(test1 < test2);
+// console.log(test1 > test2);
+// console.log(test1 == test2);
+// console.log(test1 == test3);
+// console.log(test2 == test3);
+// console.log(test2 === test3);
+// console.log(test1 <= test2);
+// console.log(test1 >= test2);
+// console.log(test1 != test2);
+// console.log(!test1 == test2);
+
+
+
+// if (test1 < test2) {
+//     console.log("nilai test 1 lebih kecil dari nilai test 2");
+// } else {
+//     console.log("nilai test 1 tidak lebih kecil dari nilai test 2");
+// }
+
+const input1 = parseInt(prompt("masukan nilai ke 1: "));
+const input2 = parseInt(prompt("masukan nilai ke 2: "));
+
+// if (input1 < input2) {
+//     console.log("nilai input1 lebih kecil dari nilai input2");
+// } else {    
+//     console.log("nilai input1 tidak lebih kecil dari nilai input2");
+// }
+
+if (input1 == input2) {
+    console.log("nilai input1 sama dengan nilai input2");
+} else if (input1 < input2) {    
+    console.log("nilai input1 lebih kecil dari nilai input2");
+} else {
+    console.log("nilai input1 lebih besar dari nilai input2");
+}
+
+
+
